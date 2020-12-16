d4149b5fb754d93b148b3249f1635e0bf0509f28
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-12-16 11:12:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    const [first, second, , ] = drivers;
+    return [first, second]
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    const [, , third, last] = drivers;
+    return [third, last];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function(fare){ return int * fare;};
+}
+
+function fareDoubler(fare) { 
+    return createFareMultiplier(2)(fare); 
+}
+
+function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(drivers, funk) {
+    return funk(drivers);
+}
